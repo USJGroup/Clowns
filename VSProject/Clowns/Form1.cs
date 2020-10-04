b99db90fc75657ee9eccc1cf66caa3efedf12586
Dumbstuff <61812799+DumbstuffTC@users.noreply.github.com>
2020-10-04 12:44:10 -0400

    Add files via upload

---
+++ VSProject/Clowns/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Diagnostics;
+
+namespace Clowns
+{
+    public partial class Form1 : Form
+    {
+
+        bool playingErrors = false;
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            for ( ; ; )
+            {
+                await Task.Delay(1000);
+                label2.Visible = false;
+                await Task.Delay(1000);
+                label2.Visible = true;
+            }
+        }
+
+        private async void closeButtonPress(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            Process.Start(Process.GetCurrentProcess().ProcessName);
+            if (playingErrors == true) return;
+            else
+            {
+                playingErrors = true;
+                for ( ; ; )
+                {
+                    await Task.Delay(1000);
+                    SystemSounds.Hand.Play();
+                    await Task.Delay(1000);
+                    SystemSounds.Asterisk.Play();
+                }
+            }
+        }
+    }
+}
